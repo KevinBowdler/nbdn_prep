5ecd09384f1d73555abc27c8f4adde68d4df8e25
Paul Houlgate <paul@kclani.com>
2010-08-31 12:25:20 +0100

    added order_by extension method to sorting routines

---
+++ product/nothinbutdotnetprep/infrastructure/sorting/OrderedEnumerable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace nothinbutdotnetprep.infrastructure.sorting
+{
+    public class OrderedEnumerable<T>:IEnumerable<T>
+    {
+        private IEnumerable<T> items;
+        private IComparer<T> comparer;
+
+        public OrderedEnumerable(IEnumerable<T> items, IComparer<T> comparer)
+        {
+            this.items = items;
+            this.comparer = comparer;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var list = new List<T>(items);
+
+            list.Sort(comparer);
+
+            foreach (var item in list)
+            {
+                yield return item;
+            }
+        }   
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public OrderedEnumerable<T> then_by<PropertyType>(Func<T,PropertyType> func) where PropertyType :IComparable<PropertyType>
+        {
+            return new OrderedEnumerable<T>(items, new ChainedComparer<T>(comparer, new PropertyComparer<T,PropertyType>(func, new ComparableComparer<PropertyType>())));
+        }
+    }
+}
---
+++ product/nothinbutdotnetprep/infrastructure/sorting/SortingExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace nothinbutdotnetprep.infrastructure.sorting
+{
+    public static class SortingExtensions
+    {
+        public static OrderedEnumerable<T> order_by<T, PropertyType>(this IEnumerable<T> items, Func<T, PropertyType> func, params PropertyType[] sortOrder)
+        {
+            return new OrderedEnumerable<T>(items, Sort<T>.by(func, sortOrder));
+        }
+
+    }
+}
